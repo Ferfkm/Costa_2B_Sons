df1906ffc363f8e1b40ec49abc4bdb21e36d8889
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-11-06 10:27:24 -0300

    idaudio2

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomAplausos;
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom(idAudio)
+    }
     contador = contador + 1;
-    console.log(contador);
 }
