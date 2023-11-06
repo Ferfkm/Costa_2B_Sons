23132d9aa5209674d8eaad568ffee3312972d054
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-11-06 10:20:39 -0300

    idaudio

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function tocaSomAplausos(){
-    document.querySelector("#som_tecla_aplausos").play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
-while(contador < 9 ){
+while(contador < listaDeTeclas.length){
     listaDeTeclas[contador].onclick = tocaSomAplausos;
     contador = contador + 1;
     console.log(contador);
